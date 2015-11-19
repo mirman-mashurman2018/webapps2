5e93d86d097318248a45af19e84cc987806e005e
mirman-mashurman2018 <mason02@mirman.org>
2015-11-19 15:09:18 -0800

    Into the 5ire saving works

--- 5_Into-the-Fire/js/feedback.js
+++ 5_Into-the-Fire/js/feedback.js
@@ -1,19 +1,22 @@
 var ref = new Firebase("https://intothe5ire.firebaseio.com"); //Use your app's Firebase URL
-
+var usersRef = ref.child("users");
 // Add some event handlers for FB events
 
 $(document).ready(function(){
+  ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+  $("#feedback-list").append(snapshot.val());
+  }, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+  });
   console.log("Document loaded");
   $("#add-form").submit(function(event){
     event.preventDefault();
   var message=$("#message").val();
-  $.post(ref,message,function(){});
+//  $.post(ref,message,function(){});
     console.log("Form submitted!");
-    ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+usersRef.push(message)
     // Go grab your info and do something with it.
   });
+
 });
