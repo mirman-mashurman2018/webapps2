0fb7aba47ed2cb67e433572cb2106f37e4deaef4
mirman-mashurman2018 <mason02@mirman.org>
2015-12-03 08:01:03 -0800

    Finished 5ire

--- 5_Into-the-Fire/js/feedback.js
+++ 5_Into-the-Fire/js/feedback.js
@@ -1,21 +1,53 @@
 var ref = new Firebase("https://intothe5ire.firebaseio.com"); //Use your app's Firebase URL
-var usersRef = ref.child("users");
 // Add some event handlers for FB events
 
 $(document).ready(function(){
   ref.on("value", function(snapshot) {
+  //  $("#feedback-list")="";
   console.log(snapshot.val());
-  $("#feedback-list").append(snapshot.val());
+  var snapstuffs=snapshot.val();
+      $("#feedback-list").empty();
+  for(var i in snapstuffs)
+  {
+    var message=snapstuffs[i];
+
+    var messageDate = new Date(message.Timestamp);
+    var newdiv=$(document.createElement("div"));
+    var otherdiv=$(document.createElement("div"));
+    otherdiv.addClass("small-12 columns");
+    newdiv.addClass("small-12 rows");
+    newdiv.append("<h2> Message:</h2>"+"<p>"+ message.Message +"</p>");
+    newdiv.append("<h2>Name: "+message.Name+"</h2>");
+    newdiv.append("<img src=\""+message.Url+"\">");
+    otherdiv.append(newdiv);
+  //  newLi.append("<p> Posted on: "+(1+messageDate.getMonth)+"</p>");
+    $("#feedback-list").append(otherdiv);
+
+
+  }
+//  $("#feedback-list").append(snapshot.val());
   }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
   });
-  console.log("Document loaded");
+
   $("#add-form").submit(function(event){
     event.preventDefault();
   var message=$("#message").val();
+  var url=$("#imgUrl").val();
+  var name=$("#Name").val();
+  var timestamp=new Date().getTime();
+  var stuffstoputin=
+  {
+    Message:message,
+Url:url,
+Name:name,
+Timestamp:timestamp
+
+
+  }
 //  $.post(ref,message,function(){});
     console.log("Form submitted!");
-usersRef.push(message)
+ref.push(stuffstoputin);
     // Go grab your info and do something with it.
   });
 
